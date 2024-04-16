cbc15b841697b2e27f15ac3d488a0b8d95323999
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-16 21:20:40 +0200

    added products

---
+++ src/Components/ProductInfo.jsx
@@ -0,0 +1,16 @@
+const products = [
+  {
+    id: 1,
+    title: "Apple watch",
+    description: "Series 5 SE",
+    rating: "",
+    price: 529.99,
+    currency: "$",
+    imageUrl: "../Assets/Name=Watch.jpg",
+    detailedDescription: [
+      "Introducing the Apple Watch Series 5 SE, a cutting-edge blend of style, functionality, and innovation, designed to accompany you on every step of your journey. Embrace the future with this versatile smartwatch, packed with features to suit both daily needs and adventurous lifestyles. Whether you are a tech enthusiast seeking the latest gadget or someone looking to enhance your daily life with smart technology, the Apple Watch Series 5 SE offers something for everyone. Its more than just a watch its a lifestyle companion. Invest in yourself and embrace the future of wearable technology. Order the Apple Watch Series 5 SE today and take the first step towards a smarter, healthier, and more connected life.",
+    ],
+  },
+];
+
+export default products;
