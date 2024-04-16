ab6db03518c38369b5d0e99ae2e5d0609178c044
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-16 23:17:15 +0200

    added more  assets

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,19 +1,41 @@
-import Logo from "../Assets/Logo.jpg";
+import React from "react";
+import "../css/SideBar.css";
+import logo from "../Assets/Logo.svg";
+import menu from "../Assets/Menu.svg";
+import home from "../Assets/Home.svg";
+import bag from "../Assets/Bag.svg";
+import navlink from "../Assets/NavLink.svg";
 
-function SideBar() {
-    return (
-      <aside className="h-screen">
-        <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-            <div className="p-4 pb-2 flex justify-between items-center">
-            <img src={Logo} alt="" />
+const SideBar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-links-top">
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="menu-icon">
+          <div className="menu">
+            <img src={menu} alt="Menu Icon" />
+          </div>
+        </div>
+        <div className="home">
+          <div className="home">
+            <img src={home} alt="Home Icon" />
+          </div>
+        </div>
+        <div className="Bag">
+          <div className="Bag">
+            <img src={bag} alt="Bag Icon" />
+          </div>
+        </div>
+      </div>
+      <div className="nav-link-bottom">
+        <div className="nav-link-bottom">
+          <img src={navlink} alt="Nav link Icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-            </div>
-
-
-        </nav>
-      </aside>
-    );
-  }
-
-  export default SideBar ;
+export default SideBar;
