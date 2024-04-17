8a38356adb83a1ba4395b8ab7dfa67831afadc57
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-17 20:59:26 +0200

    semi done with dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -16,37 +16,67 @@ function DashBoard() {
             {/* Input-Field */}
             <div className="input-field">
               {/* Placeholder */}
-              <div className="placeholder">Apple Watch,Samsung S21,Macbook Pro,..</div>
+              <div className="placeholder">
+                Apple Watch,Samsung S21,Macbook Pro,..
+              </div>
             </div>
           </div>
         </div>
 
         {/* Content-Area */}
         <div className="content-area">
-          {/* Content-Row */}
+          {/* First Row */}
           <div className="content-row">
-            {/* Item-Card */}
-            <div className="item-card">
-              {/* Product-Image */}
-              <div className="product-image">
-                {/* Image */}
-                <div className="image">Image</div>
-              </div>
-              {/* Content */}
-              <div className="content">
-                {/* Item Name */}
-                <div className="item-name">Item Name</div>
-                {/* Small Description */}
-                <div className="small-description">Small Description</div>
-                {/* Action */}
-                <div className="action">
-                  {/* Price */}
-                  <div className="price">Price</div>
-                  {/* View */}
-                  <div className="view">View</div>
+            {Array.from({ length: 4 }, (_, index) => (
+              <div key={index} className="item-card">
+                {/* Product-Image */}
+                <div className="product-image">
+                  {/* Image */}
+                  <div className="image">Image</div>
+                </div>
+                {/* Content */}
+                <div className="content">
+                  {/* Item Name */}
+                  <div className="item-name">Item Name</div>
+                  {/* Small Description */}
+                  <div className="small-description">Small Description</div>
+                  {/* Action */}
+                  <div className="action">
+                    {/* Price */}
+                    <div className="price">Price</div>
+                    {/* View */}
+                    <div className="view">View</div>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
+          </div>
+
+          {/* Second Row */}
+          <div className="content-row">
+            {Array.from({ length: 4 }, (_, index) => (
+              <div key={index} className="item-card">
+                {/* Product-Image */}
+                <div className="product-image">
+                  {/* Image */}
+                  <div className="image">Image</div>
+                </div>
+                {/* Content */}
+                <div className="content">
+                  {/* Item Name */}
+                  <div className="item-name">Item Name</div>
+                  {/* Small Description */}
+                  <div className="small-description">Small Description</div>
+                  {/* Action */}
+                  <div className="action">
+                    {/* Price */}
+                    <div className="price">Price</div>
+                    {/* View */}
+                    <div className="view">View</div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
