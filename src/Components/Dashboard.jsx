7d8ce24e34b04f7aa372816f64dcf7dfc37ed3ea
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-17 12:51:42 +0200

    updated sidebar

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import SideBar from "./SideBar";
 import SideBar2 from "./Sidebar2";
+import "../css/Dashboard.css";
 
 function DashBoard() {
   return (
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -4,11 +4,11 @@ import logo from "../Assets/Logo.svg";
 import menu from "../Assets/Menu.svg";
 import closeMenu from "../Assets/CloseMenu.svg";
 import home from "../Assets/Home.svg";
-import closeHome from "../Assets/CloseHome.svg"; // Import the close home icon
+import closeHome from "../Assets/CloseHome.svg";
 import bag from "../Assets/Bag.svg";
-import closeBag from "../Assets/CloseBag.svg"; // Import the close bag icon
+import closeBag from "../Assets/CloseBag.svg";
 import navlink from "../Assets/NavLink.svg";
-import closeNavLink from "../Assets/CloseNavLink.svg"; // Import the close nav link icon
+import closeNavLink from "../Assets/CloseNavLink.svg";
 
 const SideBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,26 +22,21 @@ const SideBar = () => {
       <div className={`nav-links-top ${isMenuOpen ? "open" : ""}`}>
         <div className="logo">
           <img src={isMenuOpen ? logo : logo} alt="Logo" />{" "}
-          {/* Change the logo icon dynamically */}
         </div>
         <div className="menu-icon" onClick={handleMenuClick}>
           <div className="menu">
             <img src={isMenuOpen ? closeMenu : menu} alt="Menu Icon" />{" "}
-            {/* Change the menu icon dynamically */}
           </div>
         </div>
         <div className="home">
           <img src={isMenuOpen ? closeHome : home} alt="Home Icon" />{" "}
-          {/* Change the home icon dynamically */}
         </div>
         <div className="Bag">
           <img src={isMenuOpen ? closeBag : bag} alt="Bag Icon" />{" "}
-          {/* Change the bag icon dynamically */}
         </div>
       </div>
       <div className="nav-link-bottom">
         <img src={isMenuOpen ? closeNavLink : navlink} alt="Nav link Icon" />{" "}
-        {/* Change the nav link icon dynamically */}
       </div>
     </div>
   );
