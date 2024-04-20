12c34d32d67b51e851490b2bfcd0d154961292a9
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-20 14:15:47 +0200

    changes

--- src/Components/Bag.jsx
+++ src/Components/Bag.jsx
@@ -6,12 +6,7 @@ import "../css/ShoppingCart.css";
 import products from "./ProductInfo";
 import bagadd from "../Assets/Bag add.svg";
 
-
-
-
-
 function Bag() {
-
   const [cartItems, setCartItems] = useState([]);
 
   const addToCart = (product) => {
@@ -22,36 +17,38 @@ function Bag() {
     setCartItems(cartItems.filter((item) => item.id !== productId));
   };
 
-    return (
-      <> <div className="shopping-bag">
-      <h2>Shopping Bag</h2>
-      {cartItems.length === 0 ? (
-        <p className="empty-message">Your bag is empty.</p>
-      ) : (
-        <div className="cart-items">
-          {cartItems.map((item) => (
-            <div key={item.id} className="item-card">
-              <div className="product-image">
-                <img src={item.imageUrl} alt={item.title} />
-              </div>
-              <div className="product-details">
-                <div className="title">{item.title}</div>
-                <div className="price">
-                  ${item.price}
+  return (
+    <>
+      {" "}
+      <div className="shopping-bag">
+        <h2>Shopping Bag</h2>
+        {cartItems.length === 0 ? (
+          <p className="empty-message">Your bag is empty.</p>
+        ) : (
+          <div className="cart-items">
+            {cartItems.map((item) => (
+              <div key={item.id} className="item-card">
+                <div className="product-image">
+                  <img src={item.imageUrl} alt={item.title} />
                 </div>
-                <button onClick={() => removeFromCart(item.id)}>Remove</button>
+                <div className="product-details">
+                  <div className="title">{item.title}</div>
+                  <div className="price">${item.price}</div>
+                  <button onClick={() => removeFromCart(item.id)}>
+                    Remove
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        )}
+        <div className="checkout">
+          <button disabled={cartItems.length === 0}>Checkout</button>
         </div>
-      )}
-      <div className="checkout">
-        <button disabled={cartItems.length === 0}>Checkout</button>
       </div>
-    </div>
-      
-       <SideBar2/></>
-    );
-  }
-  
-  export default Bag ;
+      <SideBar2 />
+    </>
+  );
+}
+
+export default Bag;
